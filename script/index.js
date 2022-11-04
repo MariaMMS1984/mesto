2f932a593fac8725dadf4dd5493ad9560c761548
Maria Matusovskaya <Mar1a13572@yandex.ru>
2022-11-04 10:30:03 +0300

    комментарий к коммиту

--- script/index.js
+++ script/index.js
@@ -3,29 +3,29 @@ const openPopupButtonElem = document.querySelector('.profile__edit-button');
 const closePopupButtonElem = document.querySelector('.popup__close-button');
 const savePopupButtonElem = document.querySelector('.popup__save-button');
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__profession');
-qs('.popup__name').value = qs('.profile__name').textContent;
-qs('.popup__profession').value = qs('.profile__profession').textContent;
+let nameInput = document.querySelector('.popup__input_data_name');
+let jobInput = document.querySelector('.popup__input_data_profession');
+let profileName = document.querySelector('.profile__name');
+let profileProfession = document.querySelector('.profile__profession');
 
-function qs(selector) {
-    return document.querySelector(selector);
-}
+
+
+
+openPopupButtonElem.addEventListener('click', () => {
+    popupElem.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileProfession.textContent;
+});
+
+closePopupButtonElem.addEventListener('click', 'submit', () => {
+    popupElem.classList.remove('popup_opened');
+});
+
+
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    qs('.profile__name').textContent = qs('.popup__name').value;
-    qs('.profile__profession').textContent = qs('.popup__profession').value;
+    profileName.textContent = nameInput.value;
+    profileProfession.textContent = jobInput.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-openPopupButtonElem.addEventListener('click', () => {
-    popupElem.classList.add('popup_opened');
-});
-
-closePopupButtonElem.addEventListener('click', () => {
-    popupElem.classList.remove('popup_opened');
-});
-
-savePopupButtonElem.addEventListener('click', () => {
-    popupElem.classList.remove('popup_opened');
-});
