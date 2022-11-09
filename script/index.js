46dcf6bc49df8873b1c07a2c29f8add0fcd2cc44
Maria Matusovskaya <Mar1a13572@yandex.ru>
2022-11-09 04:27:24 +0300

    комментарий к коммиту

--- script/index.js
+++ script/index.js
@@ -1,31 +1,38 @@
 const popupElem = document.querySelector('.popup');
 const openPopupButtonElem = document.querySelector('.profile__edit-button');
 const closePopupButtonElem = document.querySelector('.popup__close-button');
-const savePopupButtonElem = document.querySelector('.popup__save-button');
+const heartElem = document.querySelectorAll('.card__like-button');
 let formElement = document.querySelector('.popup__container');
 let nameInput = document.querySelector('.popup__input_data_name');
 let jobInput = document.querySelector('.popup__input_data_profession');
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__profession');
 
-
-
-
-openPopupButtonElem.addEventListener('click', () => {
-    popupElem.classList.add('popup_opened');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileProfession.textContent;
-});
-
-closePopupButtonElem.addEventListener('click', 'submit', () => {
-    popupElem.classList.remove('popup_opened');
-});
-
-
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileProfession.textContent = jobInput.value;
+    popupElem.classList.remove('popup_opened');
 }
 
+function openpopup() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileProfession.textContent;
+    popupElem.classList.add('popup_opened');
+}
+
+function closepopup() {
+    popupElem.classList.remove('popup_opened');
+}
+
+document.querySelectorAll('.card__like-button').forEach(heartElem => {
+    heartElem.addEventListener('click', () => {
+        heartElem.classList.toggle('card__like-button_active');
+    })
+})
+
+openPopupButtonElem.addEventListener('click', openpopup);
+
 formElement.addEventListener('submit', formSubmitHandler);
+
+closePopupButtonElem.addEventListener('click', closepopup);
